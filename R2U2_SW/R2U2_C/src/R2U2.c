9a2fe7db11aa7f6388e8b4bdeda20ecf2eb27dc9
Brian Kempa <bckempa@iastate.edu>
2019-03-06 14:13:15 -0600

    Update for new folder layout

--- R2U2_SW/R2U2_C/src/R2U2.c
+++ R2U2_SW/R2U2_C/src/R2U2.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include "R2U2Config.h"
 
-#include "TL/TL_observers.h"
-#include "AT/at_checkers.h"
-
-int main(int argc, char const *argv[])
-{
-    printf("Hello, world!\n");
+int main(int argc, char const *argv[]) {
     fprintf(stdout,"%s Version %d.%d\n",
             argv[0],
             R2U2_C_VERSION_MAJOR,
             R2U2_C_VERSION_MINOR);
+
     return 0;
 }
