195821725efa3584b7d04c76e992eb639c708b10
Brian Kempa <bckempa@iastate.edu>
2019-03-13 12:48:07 -0500

    Basic TL Running

--- R2U2_SW/R2U2_C/src/R2U2.c
+++ R2U2_SW/R2U2_C/src/R2U2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "R2U2Config.h"
+#include "TL/TL_observers.h"
+//#include "AT/at_checkers.h"; AT Disabled during refactor BCSK
 
 int main(int argc, char const *argv[]) {
     fprintf(stdout,"%s Version %d.%d\n",
@@ -7,5 +11,53 @@ int main(int argc, char const *argv[]) {
             R2U2_C_VERSION_MAJOR,
             R2U2_C_VERSION_MINOR);
 
+
+    /* Get Config */
+    static int MAX_TIME = 5; /* TODO: Replace with sensor data */
+    static int NUM_SIG = 2;    /* TODO: Replace with sensor data */
+    r2u2_in_type in_dat[5][2] = {{0, 1},
+                                      {2, 3},
+                                      {4, 5},
+                                      {6, 7},
+                                      {8, 9}};
+
+    TL_init();
+    //at_checkers_init(); AT Disabled during refactor BCSK
+
+    /* Allocate Memory */
+    r2u2_in_type *cur_sigs;
+    cur_sigs = malloc(sizeof(r2u2_in_type) * NUM_SIG);
+    if (cur_sigs == NULL) return 1;
+    /* TODO: Alloc full results buffers - memory map to file? */
+
+    /* Open File Output  */
+    FILE *out_file, *dbg_file;
+    out_file = fopen("./R2U2.out", "w+");
+    dbg_file = fopen("./R2U2.log", "w+");
+    if ((out_file == NULL) || (dbg_file == NULL)) return 1;
+    /* TODO: Async file I/O */
+
+    /* Main processing loop */
+    for (int cur_time=0; cur_time < MAX_TIME; cur_time++) {
+
+        /* Sensor values for current time step */
+        /* TODO: Replace with memory map */
+        for (int i=0; i<NUM_SIG; i++) {cur_sigs[i] = in_dat[cur_time][i];}
+
+        /* Atomics Update */
+        //at_checkers_update(cur_sigs); AT Disabled during refactor BCSK
+
+        /* Temporal Logic Update */
+        /* TODO: Why does this require file pointers? */
+        TL_update(out_file, dbg_file);
+
+        /* Copy outputs from vectors */
+
+    }
+
+    /* TODO: GNU Plot printing */
+
+    fclose(out_file);
+    fclose(dbg_file);
     return 0;
 }
--- R2U2_SW/R2U2_C/src/R2U2Config.h
+++ R2U2_SW/R2U2_C/src/R2U2Config.h
@@ -1,2 +1,4 @@
 #define R2U2_C_VERSION_MAJOR 0
 #define R2U2_C_VERSION_MINOR 1
+
+typedef double r2u2_in_type;
