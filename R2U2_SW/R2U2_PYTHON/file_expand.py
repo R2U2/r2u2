1284f29fd29f3a3cce42e1af0ca2080011b4cf29
mlmccle3 <33102870+mlmccle3@users.noreply.github.com>
2020-02-04 14:32:26 -0600

    Completed trace file expansion script
    includes file expansion script and example trace file used for testing

--- R2U2_SW/R2U2_PYTHON/file_expand.py
+++ R2U2_SW/R2U2_PYTHON/file_expand.py
@@ -1,16 +1,56 @@
-import csv
-#import array as arr
-#max is number of elements per row
+#file expansion script
+#expands atomic trace format to fill repeated data points
 
-with open('example.csv') as csvfile:
-    csv_reader = csv.reader(csv_file,delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        for i in range(max):
-	    if row[i]='  ':
-	        row[i]=rowold[line_count][i]
-	    #endif
-	#endfor
-	rowold.append(row)
-        line_count += 1
+import csv
+
+#user file input request
+file_in = input("Please input the name of the desired csv file, without the extension: ")
+file_inx = file_in + ".csv"
+print("Input atomic trace file: " + file_inx)
+
+#open atomic trace for reading
+with open(file_inx) as csvfile:
+  csv_reader = csv.reader(csvfile,delimiter=',')
+  #open blank file for writing of expanded format
+  f = open(file_in + "_exp.csv","w")
+  line_count = 0
+  
+  #finds number of columns and reset to reader to first row
+  col=len(next(csv_reader)) 
+  csvfile.seek(0)   
+
+  #create 2D list for storing data
+  stor = [[0] * col for i in range(1)] 
+
+  #reads data and replaces blank points with previous data
+  for row in csv_reader:
+    for i in range(col):
+      #tests for data represented by empty string or 1, 2, or 3 spaces
+	    if row[i] == " " or row[i] == "  " or row[i] == "   " or row[i] == "":
+        #replaces empties with data from previous line
+	      row[i] = stor[line_count][i]
+	  #endfor
+    #adds row to storage list
+    stor.append(row)
+    line_count += 1
+  #endfor
+
+  #writes storage array to expanded file
+  for j in range(line_count+1):
+    for k in range(col):
+      if j != 0:
+        if k == col-1:
+          f.write(str(stor[j][k]))
+        else:
+          f.write(str(stor[j][k]) + ',')
+        #endif
+      #endif
     #endfor
+    if j != line_count and j != 0:
+      f.write('\r')
+    #endif
+  #endfor  
+
+#closes read and write files
+csvfile.close() 
+f.close()
