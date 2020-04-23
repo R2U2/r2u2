77132ba21ab0ff713a367eb57074736534ea998c
Brian C S Kempa <bckempa@iastate.edu>
2020-04-23 00:35:27 -0400

    Comment clarity

--- R2U2_SW/R2U2_C/src/R2U2Config.h
+++ R2U2_SW/R2U2_C/src/R2U2Config.h
@@ -1,8 +1,5 @@
-#ifndef R2U2_CONGIG_H
-#define R2U2_CONGIG_H
-
-#define R2U2_C_VERSION_MAJOR 1
-#define R2U2_C_VERSION_MINOR 0
+#ifndef R2U2_CONFIG_H
+#define R2U2_CONFIG_H
 
 typedef double r2u2_input_data_t;
 
