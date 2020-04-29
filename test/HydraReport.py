6c0a6e60895f5b8e6cbf9274055f80e1efa7a074
mcauwels <mcauwels@iastate.edu>
2020-04-29 09:53:26 -0500

    Updated so that it now parses the 'HydraResults.txt' file and prints out the filenames where there are diffs

--- test/HydraReport.py
+++ test/HydraReport.py
@@ -34,3 +34,34 @@ for _oracle in oracleFiles:
             subprocess.run(['diff',__ResultCDir__+_result,__OracleDir__+_oracle],stdout=f)
             f.flush()
 f.close();
+
+# Now read the results file and print which formulas have diffs
+f = open("HydraResults.txt",'r').read()
+
+lines = f.split('\n')
+ResultsArray = []
+i = -1
+for line in lines:
+    # Ignoring blank lines,
+    if(line != ''):
+        # If the line starts with a comment,
+        if(line[0] == '#'):
+            # Pull out the file's name from the comment
+            f = line.split(' ')[3]
+            # Add a new list to the list
+            ResultsArray.append([])
+            # Update the Results Array's index
+            i = len(ResultsArray) - 1
+            # Append the filename to the ResultsArray[i]
+            ResultsArray[i].append(f)
+            # Initialize the ResultsArray's error count to zero
+            ResultsArray[i].append(0)
+        # If the line doesn't start with a comment,
+        else:
+            # Then 
+            ResultsArray[i][1] = ResultsArray[i][1] + 1
+
+
+for i in range(0,len(ResultsArray)):
+    if(ResultsArray[i][1] > 0):
+        print('Differences between Oracle and R2U2 for ' + ResultsArray[i][0])
