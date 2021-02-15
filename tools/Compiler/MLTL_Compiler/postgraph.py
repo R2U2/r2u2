7b6f728baba47e65cfa7a5cf0de827705f720dad
Chris <chrisjohannsen8@gmail.com>
2021-02-15 12:24:50 -0600

    Fixing AT default behavior

--- tools/Compiler/MLTL_Compiler/postgraph.py
+++ tools/Compiler/MLTL_Compiler/postgraph.py
@@ -73,7 +73,7 @@ class Postgraph():
         instructions = ''
         for atom in unmapped_atomics:
             num = re.search('\d+', atom).group()
-            instructions += atom + ' bool s' + num + ' 0 == 1\n'
+            instructions += atom + ': bool s' + num + ' 0 == 1\n'
 
         at_asm = self.output_path + 'at.asm'
         if os.path.isfile(at_asm):
