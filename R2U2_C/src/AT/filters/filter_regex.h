4d644ceb35329d167cf768443171dc1fb139c735
Brian C S Kempa <bckempa@iastate.edu>
2021-06-12 14:27:45 -0500

    Removes need for R2U2_STANDALONE flag

--- R2U2_C/src/AT/filters/filter_regex.h
+++ R2U2_C/src/AT/filters/filter_regex.h
@@ -18,18 +18,7 @@
 **=====================================================================================*/
 #include <regex.h>
 
-#ifndef R2U2_STANDALONE
-#define R2U2_AOS
-#endif
-
-
-#ifdef R2U2_AOS
-#include "r2u2_private_types.h"
-#endif 
 void filter_regex_init(regex_t *RE, char *RS);
 void filter_regex_free(regex_t *RE);
 int filter_regex_update_data(regex_t *RE, const char *s, char **matches);
 
-#ifdef R2U2_AOS
-int filter_regex_plexil_update_data(regex_t *RE, plexil_r2u2_msgbuf_t mb , char **matches);
-#endif
