a8dd2c7b4ed9ef2b31b3ff7d9127785647b642f9
Brian C S Kempa <bckempa@iastate.edu>
2022-02-14 17:19:35 -0600

    Make CSV header remapping more robust (and verbose) at a memory cost
    Addresses issue #187

--- R2U2_C/src/R2U2.c
+++ R2U2_C/src/R2U2.c
@@ -212,21 +212,20 @@ int signal_aux_config(char* aux, FILE* input_file, uintptr_t* alias_table){
     6: Missing a column for a listed signal
   */
   // TODO: unify this with aux file load or keep separate?
-      // If using CSV input and alias exists, set alias flag
-  // Read aux file for
   FILE *alias_file = NULL;
   uintptr_t idx, col_num;
   char type, *signal, *scan_ptr;
-  char aliasname[BUFSIZ], inbuf[BUFSIZ], line[MAX_LINE]; // LINE_MAX instead? PATH_MAX??
+  char aliasname[BUFSIZ], inbuf[BUFSIZ], hdrbuf[BUFSIZ], line[MAX_LINE]; // LINE_MAX instead? PATH_MAX??
 
   int alias_order = 0;
+  fprintf(stderr, "Remapping input signals...\n");
   if (access(aux, F_OK) == 0) {
     alias_file = fopen(aux, "r");
     if (alias_file != NULL) {
 
       /* Put CSV header (from input_file pointer) into inbuf buffer */
       if((fgets(inbuf, sizeof(inbuf), input_file) == NULL) || (inbuf[0] != '#') ){
-        fprintf(stderr, "Can't read input header\n");
+        fprintf(stderr, "\tCan't read input header\n");
         return 5;
       }
 
@@ -237,22 +236,26 @@ int signal_aux_config(char* aux, FILE* input_file, uintptr_t* alias_table){
         if(sscanf(line, "%c", &type) == 1 && type == 'S') {
           /* Found a signal definition, look for matching header */
           if(sscanf(line, "%*c %s %ld", aliasname, &idx) == 2){
-            if((signal = strstr(inbuf, aliasname)) != NULL){
-              col_num = 0;
-              for(scan_ptr=inbuf;scan_ptr != signal;scan_ptr++){
-                /* Walk through header, counting columns
-                 * Slower but lower memory than finding all columns first
-                 */
-                if(*scan_ptr == ',') {col_num += 1;}
+            strcpy(hdrbuf, inbuf+1); /* strtok nulls delims, strip # in copy */
+            col_num = 0;
+            signal = strtok(hdrbuf, ",\n");
+            while(signal != NULL){
+              if(strcmp(signal, aliasname) == 0){
+                fprintf(stderr, "\tFound signal %s in input column %lu\n", aliasname, col_num);
+                alias_table[col_num] = idx;
+                break;
+              } else {
+                signal = strtok(NULL, ",\n");
+                col_num += 1;
               }
-              alias_table[col_num] = idx;
-            } else {
-              fprintf(stderr, "No matching Column for: %s\n", aliasname);
+            }
+            if(signal == NULL){
+              fprintf(stderr, "\tNo matching Column for: %s\n", aliasname);
               return 6;
             }
           } else {
             /* Invalid signal line */
-            fprintf(stderr, "The signal mapping '%s' is invalid\n", line);
+            fprintf(stderr, "\tThe signal mapping '%s' is invalid\n", line);
             return 3;
           }
           alias_order = 1; /* Flag that reordering should be used */
