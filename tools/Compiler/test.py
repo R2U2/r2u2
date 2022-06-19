922ae9ba08fcea48f2d6cffc67ecb13b9790c19a
Chris <chrisjohannsen8@gmail.com>
2022-06-19 14:10:04 -0700

    implementing expressions

--- tools/Compiler/test.py
+++ tools/Compiler/test.py
@@ -3,7 +3,7 @@ from lark import Lark
 parser = Lark.open("MLTL.lark", rel_to=__file__, start="program")
 
 def main():
-  print(parser.parse("SPEC ♢[0]test(prop||prop)&&prop;").pretty())
+  print(parser.parse("SPEC label: ♢[0,5]prop*prop>prop+prop&&prop;").pretty())
 
 if __name__ == "__main__":
   main()
