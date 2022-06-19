f5237e79e62f07e307f4f481ed6452d91ba1f80b
Chris <chrisjohannsen8@gmail.com>
2022-06-19 00:33:30 -0700

    more testing

--- tools/Compiler/test.py
+++ tools/Compiler/test.py
@@ -3,7 +3,7 @@ from lark import Lark
 parser = Lark.open("MLTL.lark", rel_to=__file__, start="program")
 
 def main():
-  print(parser.parse("SPEC ♢[0]prop;"))
+  print(parser.parse("SPEC ♢[0]test(prop||prop)&&prop;").pretty())
 
 if __name__ == "__main__":
   main()
