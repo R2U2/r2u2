cfeed1f7800ede53015143529377b8cdad2c7312
Brian C S Kempa <bckempa@iastate.edu>
2022-06-19 21:58:09 -0700

    Ensure configuration is defined first

--- R2U2_C/src/AT/signal_set_filters/filter_all_of.h
+++ R2U2_C/src/AT/signal_set_filters/filter_all_of.h
@@ -1,11 +1,11 @@
 #ifndef FILTER_ALL_OF_H
 #define FILTER_ALL_OF_H
 
+#include "R2U2.h"
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-#include "R2U2Config.h"
 
 bool filter_all_of(bool *set, uint8_t len);
 
