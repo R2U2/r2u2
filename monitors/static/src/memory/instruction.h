281a8ad78b89593073ad0c8f9d25490a9128347c
Brian C S Kempa <bckempa@iastate.edu>
2023-09-01 03:58:54 -0700

    Fix bit-wdith of `r2u2_mltl_instruction_t` across 32 and 64 bit archs

--- monitors/static/src/memory/instruction.h
+++ monitors/static/src/memory/instruction.h
@@ -10,6 +10,10 @@ typedef struct {
   // To avoid the chicken-and-egg stand-up of the execution engines and inst memory,
   // and to allow plugin compilation of engines, we use an int instead of enum for the engine tag here
   // C99 section 6.7.2.2 §2: "The expression that defines the value of an enumeration constant shall be an integer constant expression that has a value representable as an int."
+  //
+  // This represenation will have 4 bytes of padding between the two members
+  // on 64-bit platforms, but this doesn't increaes the total size relative to
+  // reordering to place the padding at the end, so we'll eat that cost
   int engine_tag;
   void *instruction_data;
   // size_t  instruction_length;
