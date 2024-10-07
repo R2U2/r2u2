5aa3c15a721171deaf7866fc93856586d4a284d0
cgjohannsen <cgjoh@protonmail.com>
2024-10-07 14:23:54 -0500

    Making fixes for previous merge request

--- test/r2u2test.py
+++ test/r2u2test.py
@@ -279,12 +279,6 @@ class TestCase():
             str(self.test_results_dir / self.trace_path.name)
         ]
 
-        if (
-            ("booleanizer" not in self.c2po_options or not self.c2po_options["booleanizer"]) and 
-            ("atomic-checkers" not in self.c2po_options or not self.c2po_options["atomic-checkers"])  
-        ):
-            r2u2bin_command_new.append("-a")
-
         with open(self.r2u2bin_command_path, "w") as f:
             f.write(' '.join(r2u2bin_command_new))
 
