0676a9f9ac2ee19b0867552e35534613f98455ab
aurandt <aurandt@iastate.edu>
2024-10-31 23:26:44 -0400

    Allow sizing constants to be changed based on env in config.toml

--- monitors/static_rust/r2u2_core/src/internals/bounds.rs
+++ monitors/static_rust/r2u2_core/src/internals/bounds.rs
@@ -1,12 +1,15 @@
 use super::types::r2u2_float;
+use const_env::from_env;
 
-// Size of binary instruction file that can be read
-pub const SPEC_FILE_MAX_SIZE: usize = 1028;
-
+#[from_env]
 pub const R2U2_MAX_SIGNALS: usize = 256;
+#[from_env]
 pub const R2U2_MAX_ATOMICS: usize = 256;
+#[from_env]
 pub const R2U2_MAX_BZ_INSTRUCTIONS: usize = 256;
+#[from_env]
 pub const R2U2_MAX_TL_INSTRUCTIONS: usize = 256;
+#[from_env]
 pub const R2U2_TOTAL_QUEUE_MEM: usize = (256 * 1024);
 
 pub const R2U2_FLOAT_EPSILON: r2u2_float =  0.00001;
