ff1f330b862968ed6d13feb528c33af310b6d655
elizabeth <elizabeth@ubuntu.myguest.virtualbox.org>
2024-12-16 17:04:39 -0600

    add visuals for output

--- visualizer/mous3/main.py
+++ visualizer/mous3/main.py
@@ -4,6 +4,7 @@ import subprocess
 
 key_word = "invalid"
 runs = 0
+sum_fail = 0
 var_names=[]
 timestep_failures = []
 focus_var = []
@@ -52,76 +53,43 @@ for output in OUT_DIR.glob("*.out"):
         for line in file:
             if key_word in line:
                 timestep_failures[int(line[-2])]+= 1
+                sum_fail += 1
                 
             
         runs += 1
 
-print("Runs: ", runs)
-print(timestep_failures)
+
+print()
+print("TITLE OF FILE WE ARE LOOKING AT")
+
+print()
+print("{:13}".format("Time step "), end ="")
+time = 0
+for i in timestep_failures:
+    print("| {}".format(time), end = " ")
+    time += 1
+print("\n")
+
+for var in var_names:
+    if var in focus_var:
+        print("\033[43m{:12} |\033[0m".format(var[:12]))
+    else:
+        print("{:12} |".format(var[:12]))
+
+
+print("{:12}".format("Num Failures "), end = "")
+for i in timestep_failures:
+    print("| {}".format(i), end = " ")
+
+
+print("\n")
+print("\033[44mTotal Runs:     {}\033[0m".format(runs))
+print("\033[42mTotal Failures: {}\033[0m".format(sum_fail))
+print("\033[45mACG Violation:  {}\033[0m".format("PLACEHOLDER"))
+
         
 
 
 
-        # print(content)
-        #Setup Dataframe
-        # meta_data = file.readline().strip().split(',')
-        
-#BUG: There is no meta data from how script is run right now
 
-        # no_var = int(meta_data[0])
-        # no_timestep = int(meta_data[1])
-
-        # var_names = file.readline().strip().split(',')
-        # var_names.append("G")
-    
-
-        # # Add Number of time steps to columns
-        # columns = ['variable']
-        # for i in range(no_timestep):
-        #     columns.append(i)
-        # print(columns)
-
-    #     data = {} 
-
-    #     for i in range(len(var_names)):
-    #         data[i] = []
-        
-    #     data[len(var_names)-1] =[] 
-    #     fail_no = []
-    #     for line in file:
-    #         #save the outputs
-    #         if key_word in line:
-    #             fail_no.append(int(line.strip().split(' ')[-1]))
-    #             # print(fail_no)
-
-    #         if ':' in line:
-    #             com_sep = line.strip().split(',')
-    #             bool = com_sep[-1]
-    #             var_data = com_sep[0].split(':')
-
-    #             var = int(var_data[0])
-    #             time = int(var_data[1])
-
-    #             data[var].append(bool)
-
-    # # print(data)
-    # space = " "
-    # print(f"{space:3s}", end=" ")
-    # for i in range(len(data[0])):
-    #     print(f"{str(i):3s}", end=" ")
-
-    # print()
-    # count = 0
-    # var = 0
-    # for x in data:
-    #     print(f"{var_names[var]:3s}", end=" ")
-    #     for y in data[x]:
-    #         if count in fail_no:
-    #             print(f"\033[31m{y:3s}\033[0m", end=" ")
-    #         else:
-    #             print(f"{y:3s}", end=" ")
-    #         count += 1
-    #     print()
-    #     count = 0
-    #     var += 1
     
