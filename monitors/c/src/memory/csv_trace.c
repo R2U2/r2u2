63ec5270f9e45e21a12056bdf8fbf29292404241
AeRosentrater <alecrose@iastate.edu>
2025-03-06 12:51:15 -0600

    Adding event-based trace support to C version

--- monitors/c/src/memory/csv_trace.c
+++ monitors/c/src/memory/csv_trace.c
@@ -19,6 +19,17 @@ r2u2_status_t r2u2_csv_load_next_atomics(r2u2_csv_reader_t *csv_reader, r2u2_mon
 
   for(i = 0, signal = strtok(csv_reader->in_buf, ",\n"); signal; i++, signal = strtok(NULL, ",\n")) {
     // TODO(bckempa): What should the behavior be if the value isn't 1 or 0?
+
+    //if the term starts with an '@' then store it in the monitor's time_stamp
+    if (signal[0] == '@'){
+      char *timechar = signal+1;
+      uint32_t time_signature;
+      if(sscanf(timechar,"%u",&time_signature) != 1 ) return R2U2_END_OF_TRACE;
+      (monitor->time_stamp) = time_signature;
+      R2U2_DEBUG_PRINT("Event: %u\n",monitor->time_stamp);
+      continue; 
+    }
+
     r2u2_int i1;
     if(sscanf(signal, "%d", &i1) != 1) return R2U2_END_OF_TRACE;
     else ((*(monitor->atomic_buffer))[i]) = (r2u2_bool)i1;
@@ -28,7 +39,7 @@ r2u2_status_t r2u2_csv_load_next_atomics(r2u2_csv_reader_t *csv_reader, r2u2_mon
 }
 
 r2u2_status_t r2u2_csv_load_next_signals(r2u2_csv_reader_t *csv_reader, r2u2_monitor_t *monitor) {
-
+  printf("Load next signals\n");
   char *signal;
   // Since this can store a pointer, it must be able to store an index
   uintptr_t i;
@@ -57,6 +68,16 @@ r2u2_status_t r2u2_csv_load_next_signals(r2u2_csv_reader_t *csv_reader, r2u2_mon
       // Follow the pointer to the signal vector, then assign the ith element
       // Note this is a pointer into the r2u2_csv_reader_t in_buf which must
       // stay in place while the signal vector is live
+
+      //if the term starts with an '@' then store it in the monitor's time_stamp
+      if (signal[0] == '@'){
+        char *timechar = signal+1;
+        uint32_t time_signature;
+        if(sscanf(timechar,"%u",&time_signature) != 1 ) return R2U2_END_OF_TRACE;
+        (monitor->time_stamp) = time_signature;
+        R2U2_DEBUG_PRINT("Event: %u\n",monitor->time_stamp);
+        continue; 
+      }
       (*(monitor->signal_vector))[i] = signal;
     }
 
