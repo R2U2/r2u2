4f1c487b4efe41e1738386685f61efc92f8d68e2
AeRosentrater <alecrose@iastate.edu>
2025-07-16 08:31:20 -0700

    Change Aux String sizing parametrization to be based on Max Instructions

--- monitors/c/src/internals/bounds.h
+++ monitors/c/src/internals/bounds.h
@@ -11,7 +11,7 @@
 // Size of string arena, in bytes, for auxillary output
 // Only reserved if used
 #define R2U2_MAX_SIGNAL_LABEL 140 //Max length of a specification label
-#define R2U2_MAX_AUX_STRINGS R2U2_MAX_SIGNALS * R2U2_MAX_SIGNAL_LABEL
+#define R2U2_MAX_AUX_STRINGS R2U2_MAX_INSTRUCTIONS * R2U2_MAX_SIGNAL_LABEL
 
 // Future Time Memory
 //  SCQ_BYTES: arena size in bytes
