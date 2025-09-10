659d6bc20a08a20da04e687d1936b2e522bcf105
aaurandt <aurandt@iastate.edu>
2025-09-10 09:38:41 -0400

    Clean up booleanizer

--- monitors/c/src/engines/booleanizer.h
+++ monitors/c/src/engines/booleanizer.h
@@ -1,13 +1,8 @@
 #ifndef BZ_BOOLEANIZER_H
 #define BZ_BOOLEANIZER_H
 
-#include <stdint.h>
-#include <stdbool.h>
-
-#include "r2u2.h"
-#include "internals/types.h"
+#include "memory/monitor.h"
 #include "instructions/booleanizer.h"
-
 
 r2u2_status_t r2u2_bz_instruction_dispatch(r2u2_monitor_t *, r2u2_bz_instruction_t *);
 
