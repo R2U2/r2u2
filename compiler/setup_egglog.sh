be0d1ad6cdbe61c3684bcd55a2ac821ac677f396
cgjohannsen <cgjoh@protonmail.com>
2026-01-10 11:40:17 -0600

    Using https for git clone

--- compiler/setup_egglog.sh
+++ compiler/setup_egglog.sh
@@ -8,7 +8,7 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p "$SCRIPT_DIR"/deps/
-git clone git@github.com:egraphs-good/egglog-experimental.git "$SCRIPT_DIR"/deps/egglog-experimental
+git clone https://github.com/egraphs-good/egglog-experimental.git "$SCRIPT_DIR"/deps/egglog-experimental
 cd "$SCRIPT_DIR"/deps/egglog-experimental || { echo "Cannot find egglog git repo at $SCRIPT_DIR/deps/egglog-experimental" 1>&2 ; exit 2; }
 git checkout fb6401e252a4971b6cca808dec8f3c9c650ea552
 
