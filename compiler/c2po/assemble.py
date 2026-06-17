fbe5034c0854ce7aee0a1c3b58bcf636344cef22
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-06-17 15:04:40 -0400

    Bump version from 4.1.0 to 4.2.0

--- compiler/c2po/assemble.py
+++ compiler/c2po/assemble.py
@@ -1042,7 +1042,7 @@ def assemble(
 
     binary = bytes()
     binary_header = (
-        f"C2PO Version 4.1.0 for R2U2 V4.1.0 - BOM: {ENDIAN}".encode("ascii") + b"\x00"
+        f"C2PO Version 4.2.0 for R2U2 V4.2.0 - BOM: {ENDIAN}".encode("ascii") + b"\x00"
     )
     binary += CStruct("B").pack(len(binary_header) + 1) + binary_header
 
